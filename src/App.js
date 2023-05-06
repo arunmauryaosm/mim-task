ff97146cd926ad322a501b1eba1e7a2e8551574e
Arun Maurya <75794360+arunmauryaosm@users.noreply.github.com>
2023-05-07 02:37:25 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Home from './components/Home';
 import Index from './components/Index';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
-          <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Index/>}/>
-        <Route exact path="/home" element={<Home/>}/>
-      </Routes>
-      </BrowserRouter>
+        <Index/>
 
 
     </div>
